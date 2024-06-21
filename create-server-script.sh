a2482bdc065dfeee96f3625e01bba61eaa5f8a16
aman.virk.ops <aman.virk.ops@gmail.com>
2024-06-21 20:00:55 +0000

    update: main.tf and bash script updated to use credentials file from enviornment variable!

--- create-server-script.sh
+++ create-server-script.sh
@@ -3,39 +3,39 @@
 # This script is used to automate the process of creating new compute instance
 # with new disk created from snapsot
 
-while getopts d:i:c flag
+while getopts d:i flag
 do
     case "${flag}" in
         d) newdisk=${OPTARG};;
         i) ip=${OPTARG};;
-	c) credentials=${OPTARG};;
     esac
 done
 echo "New-disk: $newdisk";
 echo "IP: $ip";
-echo "Credentails File: $credentials";
+
+proj_path="$(pwd)"
+allow_ips="allow_ips=[\"${ip}\"\]"
 
 if ($newdisk == "true")
 then
     echo "Creating new disk from scratch"
-    cd disk
+    cd "${proj_path}/src/disk"
     terraform init
-    terraform plan --out main.tfplan -gcp_credentials=$credentials
+    terraform plan --out main.tfplan
     terraform apply --auto-approve main.tfplan
-    cd ..
 else
     echo "Creating new disk from snapshot"
     ## Create new disk from snapshot
-    cd disk-from-snapshot
+    cd "${proj_path}/src/disk-from-snapshot"
     terraform init
-    terraform plan --out main.tfplan -gcp_credentials=$credentials
+    terraform plan --out main.tfplan
     terraform apply --auto-approve main.tfplan
-    cd ..
 fi
 
 ## Create compute instance
-cd compute
+cd "${proj_path}/src/compute"
 terraform init
-terraform plan --out main.tfplan -gcp_credentials=$credentials -var=$ALLOW_IPS
+terraform plan --out main.tfplan -var=$allow_ips
 terraform apply --auto-approve main.tfplan
-cd ..
+
+cd "${proj_path}"
